a6021c071b379e03ef4fef7af1ac9a19e5b7e71c
Jiwon <leeji7031@gmail.com>
2023-10-10 19:19:32 +0900

    Feat : Block.cs

---
+++ Assets/Script/Block.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int block_health = 1;
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Player") && collision.transform.position.y < transform.position.y
+        && collision.gameObject.GetComponent<Player>().level > 1){
+            block_health -= 1;
+            Debug.Log(collision.gameObject.GetComponent<Player>().moveX);
+        }
+
+        if (block_health <= 0){
+            Destroy(gameObject);
+        }
+    }
+}
